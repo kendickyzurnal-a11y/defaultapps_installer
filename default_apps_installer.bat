b7fe227cf5d81e0616d44b5031ffb36f077fc006
kendickyzurnal-a11y <kendickyzurnal@gmail.com>
2026-06-19 15:42:02 +0200

    inpored file with installer

---
+++ default_apps_installer.bat
@@ -0,0 +1,24 @@
+winget install -e --id Ghisler.TotalCommander --source winget --silent --accept-package-agreements --accept-source-agreements
+winget install --exact --id MartiCliment.UniGetUI --source winget --silent --accept-package-agreements --accept-source-agreements
+winget install -e --id 7zip.7zip --source winget --silent --accept-package-agreements --accept-source-agreements
+winget install -e --id Adobe.Acrobat.Reader.64-bit --source winget --silent --accept-package-agreements --accept-source-agreements
+winget install -e --id Mozilla.Firefox.ESR.sk --source winget --silent --accept-package-agreements --accept-source-agreements
+winget install -e --id Google.Chrome --source winget --silent --accept-package-agreements --accept-source-agreements
+winget install -e --id VideoLAN.VLC --source winget --silent --accept-package-agreements --accept-source-agreements
+winget install -e --id Python.Python.3.14 --source winget --silent --accept-package-agreements --accept-source-agreements
+winget install -e --id Microsoft.VisualStudioCode --source winget --silent --accept-package-agreements --accept-source-agreements
+winget install -e --id AdrienAllard.FileConverter --source winget --silent --accept-package-agreements --accept-source-agreements
+winget install -e --id  MHNexus.HxD --source winget --silent --accept-package-agreements --accept-source-agreements
+
+winget install --id 9PFSPMJKKQHT --source msstore --accept-package-agreements --accept-source-agreements
+
+
+winget upgrade --all --silent --accept-package-agreements --accept-source-agreements
+
+
+python -m pip install --upgrade pip
+python -m pip install pipx
+
+python -m pipx ensurepath
+
+pipx install ghgrab
