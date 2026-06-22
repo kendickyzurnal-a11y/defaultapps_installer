843223301ac73c94742f893b4d73452c7b6abdb7
kendickyzurnal-a11y <kendickyzurnal@gmail.com>
2026-06-22 15:01:41 +0200

    Update default_apps_installer.bat

--- default_apps_installer.bat
+++ default_apps_installer.bat
@@ -1,5 +1,5 @@
 winget install -e --id Ghisler.TotalCommander --source winget --silent --accept-package-agreements --accept-source-agreements
-winget install --exact --id MartiCliment.UniGetUI --source winget --silent --accept-package-agreements --accept-source-agreements
+winget install --exact --id Devolutions.UniGetUI --source winget --silent --accept-package-agreements --accept-source-agreements
 winget install -e --id 7zip.7zip --source winget --silent --accept-package-agreements --accept-source-agreements
 winget install -e --id Adobe.Acrobat.Reader.64-bit --source winget --silent --accept-package-agreements --accept-source-agreements
 winget install -e --id Mozilla.Firefox.ESR.sk --source winget --silent --accept-package-agreements --accept-source-agreements
@@ -8,7 +8,11 @@ winget install -e --id VideoLAN.VLC --source winget --silent --accept-package-ag
 winget install -e --id Python.Python.3.14 --source winget --silent --accept-package-agreements --accept-source-agreements
 winget install -e --id Microsoft.VisualStudioCode --source winget --silent --accept-package-agreements --accept-source-agreements
 winget install -e --id AdrienAllard.FileConverter --source winget --silent --accept-package-agreements --accept-source-agreements
-winget install -e --id  MHNexus.HxD --source winget --silent --accept-package-agreements --accept-source-agreements
+winget install -e --id MHNexus.HxD --source winget --silent --accept-package-agreements --accept-source-agreements
+winget install -e --id rocksdanister.LivelyWallpaper --source winget --silent --accept-package-agreements --accept-source-agreements
+winget install -e --id FontForge.FontForge --source winget --silent --accept-package-agreements --accept-source-agreements
+
+
 
 winget install --id 9PFSPMJKKQHT --source msstore --accept-package-agreements --accept-source-agreements
 
